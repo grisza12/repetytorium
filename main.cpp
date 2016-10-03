8a806b1f95fd0a2d2380a3631c7b6759808911f5
grisza12 <grisza12@hotmail.com>
2016-10-03 13:19:52 +0200

    Zadanie 3

--- main.cpp
+++ main.cpp
@@ -1,19 +1,51 @@
-// This program adds two numbers and prints their sum.
 #include <iostream>
+#include <cstdio>
+#include <stdbool.h>
 using namespace std;
+
 int main()
 {
-    int a;
-    int b;
-    int sum;
+
+    char name[35];
+    char surname[35];
+    char data[50];
+    char adress[100];
+    int phone[12];
+    char email[50];
+
+    printf("Podaj imie\n");
+    scanf("%s",&name);
+    printf("Podaj nazwisko\n");
+    scanf("%s",&surname);
+    printf("Podaj date urodzenia\n");
+    scanf("%s",&data);
+    printf("Podaj adres zamieszkania\n");
+    scanf("%s",&adress);
+    printf("Podaj telefon\n");
+    scanf("%s",&phone);
+    printf("Podaj adres e-mail\n");
+    scanf("%s",&email);
+
+    printf("Imie: %s \nNazwisko: %s \nData urodzenia: %s \nAdres zamieszkania: %s\nTelefon: %d\nEmail: %s",
+           name, surname, data, adress, phone, email);
 
 
-    cin >> a ;
-    cin >> b ;
+//    int a;
+//    int b;
+//    int sum;
+//    bool p = true;
+//
+//
+//    sum = a + b;
+//
+//    puts("Witaj!");
+//    printf("Twoja ulubiona liczba: %d\n",a);
+//    printf("Jaka jest twoja ulubiona liczba calkowita?\n");
+//    scanf("%d",&a);
 
-    sum = a + b;
 
-    cout << "The sum of " << a << " and " << b << " is " << sum << "\n";
+//    cout << "Suma dwóch liczb " << a << " i " << b << " jest równa " << sum << "\n";
+//    cout << p;
 
     return 0;
 }
